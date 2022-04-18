41590db7c53330e3ec7e0a5b4e23d65f27cd4901
Vision-At-SEECS <vision@seecs.edu.pk>
2022-04-18 12:19:22 +0500

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The repository contains Machine Learning model that detects properties using Aeriel Imagery.',
+    author='RedBuffer',
+    license='',
+)
